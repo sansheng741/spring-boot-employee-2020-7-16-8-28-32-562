684b3bee31767f6c01c290f54dafd719ccc77529
sansheng <807156237@qq.com>
2020-08-01 14:30:41 +0800

    [spring boot employee] [colin] test :  should_return_company_when_getCompany_given_company_id

--- src/main/resources/db/migration/V20200801102836__add_employee.sql
+++
@@ -1,1 +0,0 @@
-insert into employee(name,age,gender,company_id) values('cc',22,'female',1)
--- src/test/java/com/thoughtworks/springbootemployee/integrationTest/EmployeeIntegrationTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/integrationTest/EmployeeIntegrationTest.java
@@ -4,22 +4,19 @@ import com.thoughtworks.springbootemployee.entity.Company;
 import com.thoughtworks.springbootemployee.entity.Employee;
 import com.thoughtworks.springbootemployee.repository.CompanyRepository;
 import com.thoughtworks.springbootemployee.repository.EmployeeRepository;
-import org.junit.After;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Profile;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -82,12 +79,13 @@ public class EmployeeIntegrationTest {
     }
 
     @Test
-    void should() {
-        //given
+    void should_return_company_when_getCompany_given_company_id() throws Exception {
+        Company company = new Company("oocl");
+        Company savedCompany = companyRepository.save(company);
+        Employee savedEmployee = employeeRepository.save(new Employee("colin", "male", 22, savedCompany));
 
-        //when
-
-        //then
-
+        mockMvc.perform(MockMvcRequestBuilders.get("/employees/" + savedEmployee.getId()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("name").value("colin"));
     }
 }
